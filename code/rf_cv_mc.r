f05451c6a60e41c0eeb841049996575556a809f9
fheleb <helebrand.frantisek@gmail.com>
2022-03-16 18:03:18 +0100

    Paralyzování všech třech stromů

--- code/rf_cv_mc.r
+++ code/rf_cv_mc.r
@@ -14,14 +14,27 @@ mtry_val = 1:(ncol(train) - 1)
 folds = sample( rep_len(1:nfolds, nrow(train)), nrow(train) )
 cv_df = data.frame(mtry = mtry_val, incorrect = rep(0, length(mtry_val)))
 cv_pars = expand.grid(mtry = mtry_val, f = 1:nfolds)
+
+
+###
 fold_err = function(i, cv_pars, folds, train) {
   mtry = cv_pars[i, "mtry"]
   fold = (folds == cv_pars[i, "f"])
-  rf.all = randomForest(lettr ~ ., train[!fold, ], ntree = ntree,
-                        mtry = mtry, norm.votes = FALSE)
+  #rf.all = randomForest(lettr ~ ., train[!fold, ], ntree = ntree,
+  #                      mtry = mtry, norm.votes = FALSE)
+  ### FH 16.3.2022 18:00
+  rf.out = mclapply(ntree, 
+                    function(x) randomForest(lettr ~ .,
+                                             train, ntree=x, mtry = mtry,
+                                             norm.votes = FALSE),
+                    mc.cores = nc)
+  rf.all = do.call(combine, rf.out)
+  ### FH 16.3.2022 18:00
+  
   pred = predict(rf.all, train[fold, ])
   sum(pred != train$lettr[fold])
 }
+
 
 nc = as.numeric(commandArgs(TRUE)[1])
 cat("Running with", nc, "cores\n")
@@ -30,14 +43,31 @@ cv_err = parallel::mclapply(1:nrow(cv_pars), fold_err, cv_pars, folds = folds,
                               train = train, mc.cores = nc) 
 err = tapply(unlist(cv_err), cv_pars[, "mtry"], sum)
 })
+
 pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 
-rf.all = randomForest(lettr ~ ., train, ntree = ntree)
+#rf.all = randomForest(lettr ~ ., train, ntree = ntree)
+### FH 16.3.2022 18:00
+rf.out = mclapply(ntree, 
+                  function(x) randomForest(lettr ~ .,
+                                           train, ntree=x,
+                                           norm.votes = FALSE),
+                  mc.cores = nc)
+rf.all = do.call(combine, rf.out)
+### FH 16.3.2022 18:00
 pred = predict(rf.all, test)
 correct = sum(pred == test$lettr)
 
 mtry = mtry_val[which.min(err)]
-rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
+#rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
+### FH 16.3.2022 18:00
+rf.out = mclapply(ntree, 
+                  function(x) randomForest(lettr ~ .,
+                                           train, ntree=x, mtry = mtry,
+                                           norm.votes = FALSE),
+                  mc.cores = nc)
+rf.all = do.call(combine, rf.out)
+### FH 16.3.2022 18:00
 pred_cv = predict(rf.all, test)
 correct_cv = sum(pred_cv == test$lettr)
 cat("Proportion Correct: ", correct/n_test, "(mtry = ", floor((ncol(test) - 1)/3),
