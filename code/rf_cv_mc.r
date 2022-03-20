63bae7ef7ce8f89e9169a139f902fe931457ac62
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-03-20 19:20:57 +0100

    porovnavam vypocetni cas oproti puvodnimu kodu dle libosti mi to zakomentujte

--- code/rf_cv_mc.r
+++ code/rf_cv_mc.r
@@ -63,10 +63,18 @@ pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 #----------------------------
 #Jara zkousi
 
-ntree_new = lapply(splitIndices(200, nc), length)
-rf = function(x) randomForest(lettr ~ ., train, ntree=x, norm.votes = FALSE)
-rf.out = mclapply(ntree_new, rf, mc.cores = nc)
-rf.all = do.call(combine, rf.out)
+#ntree_new = lapply(splitIndices(200, nc), length)
+#rf = function(x) randomForest(lettr ~ ., train, ntree=x, norm.votes = FALSE)
+#rf.out = mclapply(ntree_new, rf, mc.cores = nc)
+#rf.all = do.call(combine, rf.out)
+
+#puvodni cast kodu 
+rf.all = randomForest(lettr ~ ., train, ntree = ntree)
+pred = predict(rf.all, test)
+correct = sum(pred == test$lettr)
+
+
+#konci jaruv kod klidne si ho zakomentujte
 
 #----------------------------
 
