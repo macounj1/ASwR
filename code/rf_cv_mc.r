cd35e17a58606fc2b137c28f76d4881b7efd06fb
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-03-21 14:13:11 +0100

    snad již funkcni kod paralelizovany 2. randomforrest

--- code/rf_cv_mc.r
+++ code/rf_cv_mc.r
@@ -37,17 +37,28 @@ err = tapply(unlist(cv_err), cv_pars[, "mtry"], sum)
 
 pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 
+#-----------------------------------------------------------
 #rf.all = randomForest(lettr ~ ., train, ntree = ntree)
 ### FH 21.3.2022 13:35
-rf.out = mclapply(ntree, 
-                  function(x) randomForest(lettr ~ .,
-                                           train, ntree=x,
-                                           norm.votes = FALSE),
-                  mc.cores = nc)
-rf.all = do.call(combine, rf.out)
+#rf.out = mclapply(ntree, 
+#                  function(x) randomForest(lettr ~ .,
+#                                           train, ntree=x,
+#                                           norm.votes = FALSE),
+#                  mc.cores = nc)
+#rf.all = do.call(combine, rf.out)
 ### FH 21.3.2022 13:35
-pred = predict(rf.all, test)
+#pred = predict(rf.all, test)
+
+
+#-----------------------------------------------
+#Jaruv kod paralelizace 2.volani random forrest
+ntree_new = lapply(splitIndices(200, nc), length)
+rf = function(x) randomForest(lettr ~ ., train, ntree=x, norm.votes = FALSE)
+rf.out = mclapply(ntree_new, rf, mc.cores = nc)
+rf.all = do.call(combine, rf.out)
 correct = sum(pred == test$lettr)
+#-----------------------------------------------
+
 
 mtry = mtry_val[which.min(err)]
 rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
