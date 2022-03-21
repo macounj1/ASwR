ea955937c6870f7fad885d379d716748d06b8e83
fheleb <helebrand.frantisek@gmail.com>
2022-03-21 13:35:53 +0100

    Fanda - Para 2. stromu

--- code/rf_cv_mc.r
+++ code/rf_cv_mc.r
@@ -22,16 +22,6 @@ fold_err = function(i, cv_pars, folds, train) {
   fold = (folds == cv_pars[i, "f"])
   rf.all = randomForest(lettr ~ ., train[!fold, ], ntree = ntree,
                         mtry = mtry, norm.votes = FALSE)
-  
-  ### FH 16.3.2022 18:04
-  #rf.out = mclapply(ntree, 
-  #                  function(x) randomForest(lettr ~ .,
-  #                                           train, ntree=x, mtry = mtry,
-  #                                           norm.votes = FALSE),
-  #                  mc.cores = nc)
-  #rf.all = do.call(combine, rf.out)
-  ### FH 16.3.2022 18:04
-  
   pred = predict(rf.all, train[fold, ])
   sum(pred != train$lettr[fold])
 }
@@ -48,27 +38,19 @@ err = tapply(unlist(cv_err), cv_pars[, "mtry"], sum)
 pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 
 #rf.all = randomForest(lettr ~ ., train, ntree = ntree)
-### FH 16.3.2022 18:37
+### FH 21.3.2022 13:35
 rf.out = mclapply(ntree, 
                   function(x) randomForest(lettr ~ .,
                                            train, ntree=x,
                                            norm.votes = FALSE),
                   mc.cores = nc)
 rf.all = do.call(combine, rf.out)
-### FH 16.3.2022 18:37
+### FH 21.3.2022 13:35
 pred = predict(rf.all, test)
 correct = sum(pred == test$lettr)
 
 mtry = mtry_val[which.min(err)]
-#rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
-### FH 16.3.2022 18:00
-#rf.out = mclapply(ntree, 
-#                  function(x) randomForest(lettr ~ .,
-#                                           train, ntree=x, mtry = mtry,
-#                                           norm.votes = FALSE),
-#                  mc.cores = nc)
-#rf.all = do.call(combine, rf.out)
-### FH 16.3.2022 18:00
+rf.all = randomForest(lettr ~ ., train, ntree = ntree, mtry = mtry)
 pred_cv = predict(rf.all, test)
 correct_cv = sum(pred_cv == test$lettr)
 cat("Proportion Correct: ", correct/n_test, "(mtry = ", floor((ncol(test) - 1)/3),
