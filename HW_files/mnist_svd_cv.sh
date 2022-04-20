a27d8ef24a6bb50c8a7310a701448fe997a2b7b3
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-20 20:59:01 +0200

    skript co bezi az moc dlouho

---
+++ HW_files/mnist_svd_cv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -N mnist_svd_cv
+#PBS -l select=2:mpiprocs=64,walltime=00:12:00
+#PBS -q qexp
+#PBS -e mnist_svd_cv.e
+#PBS -o mnist_svd_cv.o
+
+cd ~/ASwR/HW_files
+pwd
+
+module load R
+echo "loaded R"
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
+
+## --args blas fork
+
+
+time mpirun --map-by ppr:32:node Rscript mnist_svd_cv.R
