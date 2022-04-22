7d2624640f450eb61dd80f621796f8252ed07361
DESKTOP-KC9TDN9\macou <macounjaromir@gmail.com>
2022-04-22 07:39:48 +0200

    finanl code

--- HW_files/HW8.R
+++ HW_files/HW8.R
@@ -1,4 +1,5 @@
 
+#' svdmod
 #' 
 #' Computes SVD for each image label in training data
 #' Returns SVDs truncated to either k components or percent variability
@@ -92,6 +93,9 @@ suppressMessages(library(pbdMPI))
 library(parallel)
 library(ggplot2)
 source("../mnist/mnist_read.R")
+source("../code/flexiblas_setup.r") 
+setback("OPENBLAS")
+setthreads(1)
 
 
 
@@ -162,14 +166,14 @@ fold_err = function(i, cv, folds, train) {
 }
 
 ## apply fold_err() over parameter combinations
-comm.print(my_index)
 
 
 
 
-comm.print("preslo to pred lapply",my.rank,all.rank = TRUE)
-my_cv_err = lapply(my_index,fold_err, cv = cv, folds = folds, train = train)
-comm.print("preslo to za lapply",my.rank,all.rank = TRUE)
+
+
+my_cv_err = mclapply(my_index,fold_err, cv = cv, folds = folds, train = train,mc.cores=4)
+
 
 
 cv_err = allgather(my_cv_err) 
@@ -182,7 +186,7 @@ cv_err_par = tapply(unlist(cv_err), cv[, "par"], sum)
 
 #cv_err_par_colect <- unlist(allgather(cv_err_par))
 ## plot cv curve with loess smoothing (ggplot default)
-comm.print(cv_err_par)
+
 
 pdf("Crossvalidation.pdf")
 ggplot(data.frame(pct = pars, error = cv_err_par/nrow(train)), 
